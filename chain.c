5c6bedede01176553752890ab964f8d7b8afba4a
thomas-citro <99105760+thomas-citro@users.noreply.github.com>
2022-02-07 02:42:54 -0600

    Update chain.c

--- chain.c
+++ chain.c
@@ -31,20 +31,20 @@ int main (int argc, char *argv[]) {
 	}
 	
 	
-	int i, n;
+	/*int i, n;
 	n = nprocs;
 	pid_t childpid = 0;
-	/*if (argc != 2){ check for valid number of command-line arguments 
+	if (argc != 2){ check for valid number of command-line arguments 
 		fprintf(stderr, "Usage: %s processes\n", argv[0]);
 		return 1;
 	}
 	n = atoi(argv[1]);
-	/*
+	
 	
 	for (i = 1; i < n; i++)
 		if (childpid = fork())
 			break;
 	fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n", i, (long)getpid(), (long)getppid(), (long)childpid);
+	*/
 	return 0;
 }
-/* This is a comment */
