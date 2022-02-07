a3fa786981d32ab7c6806f2ff44a0a7e67eeac35
thomas-citro <99105760+thomas-citro@users.noreply.github.com>
2022-02-07 02:14:48 -0600

    Update and rename simplechain.c to chain.c

--- chain.c
+++ chain.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <getopt.h>
 int main (int argc, char *argv[]) {
-	pid_t childpid = 0;
+	int option;
+	while ((option = getopt(arc, argv, "cf")) != -1) {
+		switch (option) {
+			case 'c' :
+				printf("chose c flag\n");
+			case 'f' :
+				printf("chose f flag\n");
+			default:
+				printf("Error");
+		}
+	}
+	
+	
 	int i, n;
-	if (argc != 2){ /* check for valid number of command-line arguments */
+	pid_t childpid = 0;
+	/*if (argc != 2){ check for valid number of command-line arguments 
 		fprintf(stderr, "Usage: %s processes\n", argv[0]);
 		return 1;
 	}
 	n = atoi(argv[1]);
+	/*
+	
 	for (i = 1; i < n; i++)
 		if (childpid = fork())
 			break;
