4d2e17ac8c73200815d24e147179bb8a9741deba
thomas-citro <99105760+thomas-citro@users.noreply.github.com>
2022-02-08 16:51:02 -0600

    Modified code for step 7

--- chain.c
+++ chain.c
@@ -88,7 +88,11 @@ int main (int argc, char *argv[]) {
 	for (i = 0; i < niters; i++) {
 		sleep(sleeptime);
 		wait();
-		fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n", i, (long)getpid(), (long)getppid(), (long)childpid);
+		fprintf(stderr, "i:%d ", i);
+		fprintf(stderr, "process ID:%ld ", (long)getpid());
+		fprintf(stderr, "parent ID:%ld ", (long)getppid());
+		fprintf(stderr, "child ID:%ld\n", (long)childpid);
+		/*fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n", i, (long)getpid(), (long)getppid(), (long)childpid);*/
 	}
 	
 	
