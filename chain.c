3336a79cc71c00f040a2fc1aaffbf0da90f8a284
thomas-citro <99105760+thomas-citro@users.noreply.github.com>
2022-02-07 23:03:21 -0600

    Update chain.c

--- chain.c
+++ chain.c
@@ -47,9 +47,10 @@ int main (int argc, char *argv[]) {
 	
 	char mybuf[nchars];
 	char c;
+	fprintf(stderr, "nchars == %d\n", nchars);
 	for (i = 0; i < nchars; i++) {
 		c = getc(stdin);
-		fprintf(stderr, "process: %c\n", c);
+		fprintf(stderr, "process #%d: %c\n", i, c);
 	}
 	
 	/*fscanf(stdin, "%s", mybuf);*/
